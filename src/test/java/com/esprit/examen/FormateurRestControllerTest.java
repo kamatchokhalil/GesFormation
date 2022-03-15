aaccb69d73a7e0f193a11adb47173bf2b9add69f
Oueslati Ahmed Khalil <45701691+kamatchokhalil@users.noreply.github.com>
2022-03-15 17:06:47 +0100

    cas de test
    mock

--- src/test/java/com/esprit/examen/FormateurRestControllerTest.java
+++ src/test/java/com/esprit/examen/FormateurRestControllerTest.java
@@ -3,6 +3,8 @@ package com.esprit.examen;
 import com.esprit.examen.config.LoggingAspect;
 import com.esprit.examen.entities.Formateur;
 import com.esprit.examen.services.IFormateurService;
+import jdk.net.SocketFlow;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
@@ -11,10 +13,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.transaction.Transactional;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 // les deux importation pour la méthode get au niveau de perform
 
 
@@ -92,6 +100,24 @@ class FormateurRestControllerTest extends GesFApplicationTests{
 
 
     }
+    @Test
+    void testSgetAllFormateur() {
+        try {
+            mockMvc.perform(get("/formateur"))
+                    .andExpect(status().isOk())
+                    .andExpect((ResultMatcher) content().contentType("application/json;charest=UTF8"))
+                    .andExpect(jsonPath("$.contrat").value("FREELANCE"))
+                    .andExpect(jsonPath("$.nom").value("dev"))
+                    .andExpect(jsonPath("$.prenon").value("dev"))
+                    .andExpect(jsonPath("$.poste").value("INGÉNIEUR"))
+                    .andExpect(jsonPath("$.email").value("dev@gmail.com"))
+                    .andExpect(jsonPath("$.password").value("dev"))
+                    .andExpect(jsonPath("$.admin").value("2"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     void testSupprimerFormateur() {
